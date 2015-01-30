cad1cb248c93f05dd0687fb0adab46ba546f70b9
RayBa82 <rainerbaun@gmail.com>
2015-01-30 20:10:32 +0100

    Fix requiring latest RS Version

---
+++ dvbViewerController/src/main/java/org/dvbviewer/controller/io/RecordingService.java
@@ -0,0 +1,58 @@
+package org.dvbviewer.controller.io;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.dvbviewer.controller.io.data.TargetHandler;
+import org.dvbviewer.controller.io.data.VersionHandler;
+import org.dvbviewer.controller.utils.ServerConsts;
+
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by RayBa on 30.01.2015.
+ */
+public class RecordingService {
+
+    public static String getVersionString() {
+        String version = null;
+        try {
+            String versionXml = ServerRequest.getRSString(ServerConsts.URL_VERSION);
+            VersionHandler versionHandler = new VersionHandler();
+            version = versionHandler.parse(versionXml);
+            //here is a regex required!
+            version = version.replace("DVBViewer Recording Service ", "");
+            String[] arr = version.split(" ");
+            version = arr[0];
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(RecordingService.class.getSimpleName(), "Error getting version from rs");
+        }
+        return version;
+    }
+
+    public static String getDVBViewerTargets() {
+        String jsonClients = null;
+        try {
+            String xml = ServerRequest.getRSString(ServerConsts.URL_TARGETS);
+            TargetHandler handler = new TargetHandler();
+            List<String> targets = handler.parse(xml);
+            Collections.sort(targets);
+            Type type = new TypeToken<List<String>>() {
+            }.getType();
+            Gson gson = new Gson();
+            jsonClients = gson.toJson(targets, type);
+        } catch (Exception e) {
+            Log.e(RecordingService.class.getSimpleName(), "Error getting DVBViewer Targets");
+            e.printStackTrace();
+        }
+        return jsonClients;
+    }
+
+
+}
--- dvbViewerController/src/main/java/org/dvbviewer/controller/utils/NetUtils.java
+++ dvbViewerController/src/main/java/org/dvbviewer/controller/utils/NetUtils.java
@@ -31,111 +31,111 @@ import java.net.InetAddress;
  * @date 07.04.2013
  */
 public class NetUtils {
-	
-	private static final int PORT = 9; 
-	
-	/**
-	 * Try to extract a hardware MAC address from a given IP address or machine name using the
-	 * ARP cache (/proc/net/arp).<br>
-	 * <br>
-	 * We assume that the file has this structure:<br>
-	 * <br>
-	 * IP address       HW type     Flags       HW address            Mask     Device<br>
-	 * 192.168.18.11    0x1         0x2         00:04:20:06:55:1a     *        eth0<br>
-	 * 192.168.18.36    0x1         0x2         00:22:43:ab:2a:5b     *        eth0<br>
-	 *
-	 * @param ip the ip or a machine name
-	 * @return the MAC from the ARP cache
-	 * @author RayBa
-	 * @date 07.04.2013
-	 */
-	public static String getMacFromArpCache(String ip) {
-	    if (ip == null)
-	        return null;
-	    BufferedReader br = null;
-	    try {
-	    	ip = InetAddress.getByName(ip).getHostAddress();
-	        br = new BufferedReader(new FileReader("/proc/net/arp"));
-	        String line;
-	        while ((line = br.readLine()) != null) {
-	            String[] splitted = line.split(" +");
-	            if (splitted != null && splitted.length >= 4 && ip.equals(splitted[0])) {
-	                // Basic sanity check
-	                String mac = splitted[3];
-	                if (mac.matches("..:..:..:..:..:..")) {
-	                    return mac;
-	                } else {
-	                    return null;
-	                }
-	            }
-	        }
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    } finally {
-	        try {
-	            br.close();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	    return null;
-	}
-	
-	/**
-	 * Send wake on lan.
-	 *
-	 * @param ipAddress the ip address
-	 * @param macAddress the mac address
-	 * @author RayBa
-	 * @date 07.04.2013
-	 */
-	public static void sendWakeOnLan(String macAddress, int port) {
-		Log.d(NetUtils.class.getSimpleName(), "sendWakeOnLan");
-		Log.d(NetUtils.class.getSimpleName(), "macAddress:"+macAddress);
-	    try {
-	        byte[] macBytes = getMacBytes(macAddress);
-	        byte[] bytes = new byte[6 + 16 * macBytes.length];
-	        for (int i = 0; i < 6; i++) {
-	            bytes[i] = (byte) 0xff;
-	        }
-	        for (int i = 6; i < bytes.length; i += macBytes.length) {
-	            System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
-	        }
-	        InetAddress address = InetAddress.getByName("255.255.255.255");
-	        DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, PORT);
-	        DatagramSocket socket = new DatagramSocket();
-	        socket.send(packet);
-	        socket.close();
-	    }
-	    catch (Exception e) {
-	    	e.printStackTrace();
-	    }
-	}
-	
-	/**
-	 * Gets the mac bytes.
-	 *
-	 * @param macStr the mac str
-	 * @return the mac bytes
-	 * @throws IllegalArgumentException the illegal argument exception
-	 * @author RayBa
-	 * @date 07.04.2013
-	 */
-	private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
-	    byte[] bytes = new byte[6];
-	    String[] hex = macStr.split("(\\:|\\-)");
-	    if (hex.length != 6) {
-	        throw new IllegalArgumentException("Invalid MAC address.");
-	    }
-	    try {
-	        for (int i = 0; i < 6; i++) {
-	            bytes[i] = (byte) Integer.parseInt(hex[i], 16);
-	        }
-	    }
-	    catch (NumberFormatException e) {
-	        throw new IllegalArgumentException("Invalid hex digit in MAC address.");
-	    }
-	    return bytes;
-	}
+
+    private static final int PORT = 9;
+
+    /**
+     * Try to extract a hardware MAC address from a given IP address or machine name using the
+     * ARP cache (/proc/net/arp).<br>
+     * <br>
+     * We assume that the file has this structure:<br>
+     * <br>
+     * IP address       HW type     Flags       HW address            Mask     Device<br>
+     * 192.168.18.11    0x1         0x2         00:04:20:06:55:1a     *        eth0<br>
+     * 192.168.18.36    0x1         0x2         00:22:43:ab:2a:5b     *        eth0<br>
+     *
+     * @param ip the ip or a machine name
+     * @return the MAC from the ARP cache
+     * @author RayBa
+     * @date 07.04.2013
+     */
+    public static String getMacFromArpCache(String ip) {
+        if (ip == null)
+            return null;
+        BufferedReader br = null;
+        try {
+            ip = InetAddress.getByName(ip).getHostAddress();
+            br = new BufferedReader(new FileReader("/proc/net/arp"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] splitted = line.split(" +");
+                if (splitted != null && splitted.length >= 4 && ip.equals(splitted[0])) {
+                    // Basic sanity check
+                    String mac = splitted[3];
+                    if (mac.matches("..:..:..:..:..:..")) {
+                        return mac;
+                    } else {
+                        return null;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Send wake on lan.
+     *
+     * @param macAddress the MACaddress
+     * @param macAddress the mac address
+     * @author RayBa
+     * @date 07.04.2013
+     */
+    public static void sendWakeOnLan(String macAddress, int port) {
+        Log.d(NetUtils.class.getSimpleName(), "sendWakeOnLan");
+        Log.d(NetUtils.class.getSimpleName(), "macAddress:" + macAddress);
+        try {
+            byte[] macBytes = getMacBytes(macAddress);
+            byte[] bytes = new byte[6 + 16 * macBytes.length];
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) 0xff;
+            }
+            for (int i = 6; i < bytes.length; i += macBytes.length) {
+                System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
+            }
+            InetAddress address = InetAddress.getByName("255.255.255.255");
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, PORT);
+            DatagramSocket socket = new DatagramSocket();
+            socket.send(packet);
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Gets the mac bytes.
+     *
+     * @param macStr the mac str
+     * @return the mac bytes
+     * @throws IllegalArgumentException the illegal argument exception
+     * @author RayBa
+     * @date 07.04.2013
+     */
+    private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
+        byte[] bytes = new byte[6];
+        String[] hex = macStr.split("(\\:|\\-)");
+        if (hex.length != 6) {
+            throw new IllegalArgumentException("Invalid MAC address.");
+        }
+        try {
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) Integer.parseInt(hex[i], 16);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid hex digit in MAC address.");
+        }
+        return bytes;
+    }
 
 }
