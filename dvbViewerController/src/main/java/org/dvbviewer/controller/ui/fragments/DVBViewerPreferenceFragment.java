692e925765c7a536b771fffa98daa9b6a1855a3c
RayBa82 <rainerbaun@gmail.com>
2015-12-23 11:57:19 +0100

    Issue #27 - replace 3rd party preference  lib with support lib

--- dvbViewerController/src/main/java/org/dvbviewer/controller/ui/fragments/DVBViewerPreferenceFragment.java
+++ dvbViewerController/src/main/java/org/dvbviewer/controller/ui/fragments/DVBViewerPreferenceFragment.java
@@ -1,35 +1,19 @@
 package org.dvbviewer.controller.ui.fragments;
 
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceClickListener;
-import android.preference.PreferenceManager;
-import android.support.v4.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
 
 import org.dvbviewer.controller.R;
 import org.dvbviewer.controller.entities.DVBViewerPreferences;
 
-public class DVBViewerPreferenceFragment extends  PreferenceFragment implements OnPreferenceClickListener{
+public class DVBViewerPreferenceFragment extends PreferenceFragmentCompat {
 	
 	@Override
-	public void onCreate(Bundle paramBundle) {
-		super.onCreate(paramBundle);
+	public void onCreatePreferences(Bundle bundle, String s) {
 		PreferenceManager prefMgr = getPreferenceManager();
 		prefMgr.setSharedPreferencesName(DVBViewerPreferences.PREFS);
-
 		addPreferencesFromResource(R.xml.preferences);
-		Preference rsSettings = (Preference) findPreference(DVBViewerPreferences.KEY_RS_SETTINGS);
-		rsSettings.setOnPreferenceClickListener(this);
 	}
-
-	@Override
-	public boolean onPreferenceClick(Preference preference) {
-		if (preference.getKey().equals(DVBViewerPreferences.KEY_RS_SETTINGS)) {
-			preference.getIntent().putExtra("id", R.xml.rs_preferences);
-		}
-		return false;
-	}
-
-	
 
 }
