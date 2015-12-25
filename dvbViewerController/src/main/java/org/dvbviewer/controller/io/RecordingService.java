af8595a7c6fb566d07e0542a3108e4ea1c22fa16
RayBa82 <rainerbaun@gmail.com>
2015-12-25 18:56:49 +0100

    Issue #30 - using now regex for version parsing

--- dvbViewerController/src/main/java/org/dvbviewer/controller/io/RecordingService.java
+++ dvbViewerController/src/main/java/org/dvbviewer/controller/io/RecordingService.java
@@ -12,26 +12,38 @@ import org.dvbviewer.controller.utils.ServerConsts;
 import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by RayBa on 30.01.2015.
  */
 public class RecordingService {
 
+    private static  final String TAG = RecordingService.class.getSimpleName();
+
+    private final static Pattern versionPattern    = Pattern.compile("(?!\\.)(\\d+(\\.\\d+)+)(?![\\d\\.])");
+
     public static String getVersionString() {
         String version = null;
         try {
             String versionXml = ServerRequest.getRSString(ServerConsts.REC_SERVICE_URL + ServerConsts.URL_VERSION);
             VersionHandler versionHandler = new VersionHandler();
-            version = versionHandler.parse(versionXml);
-            //here is a regex required!
-            version = version.replace("DVBViewer Recording Service ", "");
-            String[] arr = version.split(" ");
-            version = arr[0];
-
+            final String rawVersion = versionHandler.parse(versionXml);
+            Matcher matcher    = versionPattern.matcher(rawVersion);
+            boolean matchFound = matcher.find();
+            if (matchFound) {
+                version = matcher.group(1);
+                Log.d(TAG, "Version number: " + version);
+            } else {
+                // ugly fallback if anything goes wrong,
+                // this will be removed when someone wrote some unit tests for the above regex ;-)
+                version = version.replace("DVBViewer Recording Service ", "");
+                String[] arr = version.split(" ");
+                version = arr[0];
+            }
         } catch (Exception e) {
-            e.printStackTrace();
-            Log.e(RecordingService.class.getSimpleName(), "Error getting version from rs");
+            Log.e(TAG, "Error getting version from rs", e);
         }
         return version;
     }
@@ -48,8 +60,7 @@ public class RecordingService {
             Gson gson = new Gson();
             jsonClients = gson.toJson(targets, type);
         } catch (Exception e) {
-            Log.e(RecordingService.class.getSimpleName(), "Error getting DVBViewer Targets");
-            e.printStackTrace();
+            Log.e(TAG, "Error getting DVBViewer Targets", e);
         }
         return jsonClients;
     }
