a2ea093b44300906a723c10361a029a061b7d91d
RayBa82 <org.dvbviewer@gmail.com>
2019-08-18 17:16:57 +0200

    small fixes

--- dvbViewerController/src/main/java/org/dvbviewer/controller/activitiy/DrawerActivity.kt
+++ dvbViewerController/src/main/java/org/dvbviewer/controller/activitiy/DrawerActivity.kt
@@ -3,16 +3,13 @@ package org.dvbviewer.controller.activitiy
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
-
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.cursoradapter.widget.SimpleCursorAdapter
 import androidx.drawerlayout.widget.DrawerLayout
-
 import org.dvbviewer.controller.R
 import org.dvbviewer.controller.data.ProviderConsts
 import org.dvbviewer.controller.ui.base.BaseActivity
@@ -21,7 +18,7 @@ abstract class DrawerActivity : BaseActivity(), OnItemClickListener {
 
     protected lateinit var mDrawerLayout: DrawerLayout
     protected lateinit var mDrawerList: ListView
-    private var mDrawerToggle: ActionBarDrawerToggle? = null
+    private lateinit var mDrawerToggle: ActionBarDrawerToggle
     protected lateinit var mDrawerAdapter: SimpleCursorAdapter
 
     /*
@@ -33,9 +30,9 @@ abstract class DrawerActivity : BaseActivity(), OnItemClickListener {
 	 */
     public override fun onCreate(arg0: Bundle?) {
         super.onCreate(arg0)
-        mDrawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        mDrawerLayout = findViewById(R.id.drawer_layout)
         mDrawerLayout.setDrawerShadow(android.R.color.white, GravityCompat.END)
-        mDrawerList = findViewById<View>(R.id.left_drawer) as ListView
+        mDrawerList = findViewById(R.id.left_drawer)
         mDrawerList.onItemClickListener = this
         val mToolbar = findViewById<Toolbar>(R.id.toolbar)
         mToolbar?.let { setSupportActionBar(it) }
@@ -43,7 +40,7 @@ abstract class DrawerActivity : BaseActivity(), OnItemClickListener {
                 this, mDrawerLayout, mToolbar,
                 R.string.app_name, R.string.app_name
         )
-        mDrawerLayout.addDrawerListener(mDrawerToggle!!)
+        mDrawerLayout.addDrawerListener(mDrawerToggle)
         setDisplayHomeAsUpEnabled(true)
 
         mDrawerAdapter = SimpleCursorAdapter(applicationContext, R.layout.list_item_group, null, arrayOf(ProviderConsts.GroupTbl.NAME), intArrayOf(android.R.id.text1), 0)
@@ -52,33 +49,33 @@ abstract class DrawerActivity : BaseActivity(), OnItemClickListener {
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
-        mDrawerToggle!!.syncState()
+        mDrawerToggle.syncState()
     }
 
     protected fun setDrawerEnabled(enabled: Boolean) {
         if (enabled) {
             mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-            mDrawerToggle!!.onDrawerStateChanged(DrawerLayout.STATE_IDLE)
-            mDrawerToggle!!.isDrawerIndicatorEnabled = true
-            mDrawerToggle!!.syncState()
+            mDrawerToggle.onDrawerStateChanged(DrawerLayout.STATE_IDLE)
+            mDrawerToggle.isDrawerIndicatorEnabled = true
+            mDrawerToggle.syncState()
 
         } else {
             mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
-            mDrawerToggle!!.onDrawerStateChanged(DrawerLayout.STATE_IDLE)
-            mDrawerToggle!!.isDrawerIndicatorEnabled = false
-            mDrawerToggle!!.syncState()
+            mDrawerToggle.onDrawerStateChanged(DrawerLayout.STATE_IDLE)
+            mDrawerToggle.isDrawerIndicatorEnabled = false
+            mDrawerToggle.syncState()
         }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        mDrawerToggle!!.onConfigurationChanged(newConfig)
+        mDrawerToggle.onConfigurationChanged(newConfig)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Pass the event to ActionBarDrawerToggle, if it returns
         // true, then it has handled the app icon touch event
-        return if (mDrawerToggle!!.onOptionsItemSelected(item)) {
+        return if (mDrawerToggle.onOptionsItemSelected(item)) {
             true
         } else super.onOptionsItemSelected(item)
         // Handle your other action bar items...
